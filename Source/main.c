bdbd5b590674b42d8cbc8bb8440ba0bc705114fa
IrvinRamiA <iramirezaltammx@gmail.com>
2020-05-24 21:35:23 -0500

    Added 2 taks

--- Source/main.c
+++ Source/main.c
@@ -1,19 +1,50 @@
-/**
-  ******************************************************************************
+/*!
   * @file    main.c
-  * @author  Ac6
-  * @version V1.0
-  * @date    01-December-2013
-  * @brief   Default main function.
-  ******************************************************************************
-*/
-
+  * @brief   Default main function
+  */
 
 #include "stm32f4xx.h"
+#include "FreeRTOS.h"
+#include "task.h"
 			
+TaskHandle_t xTaskHandle1 = NULL;
+TaskHandle_t xTaskHandle2 = NULL;
+
+void Task1Handler(void *params);
+void Task2Handler(void *params);
 
 int main(void)
 {
+	RCC_DeInit();
+	SystemCoreClockUpdate();
 
-	for(;;);
+	xTaskCreate(
+		Task1Handler,
+		"Task-1",
+		configMINIMAL_STACK_SIZE,
+		NULL,
+		2,
+		&xTaskHandle1);
+
+	xTaskCreate(
+		Task2Handler,
+		"Task2",
+		configMINIMAL_STACK_SIZE,
+		NULL,
+		2,
+		&xTaskHandle2);
+
+	vTaskStartScheduler();
+
+	while(1);
+}
+
+void Task1Handler(void *params)
+{
+	while(1);
+}
+
+void Task2Handler(void *params)
+{
+	while(1);
 }
